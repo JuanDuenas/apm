ac28f5f49e37c913dd5e3be0f27d4e7f1acd888f
Juan Pablo <duenasjuanpablo1939@gmail.com>
2026-04-12 03:41:23 -0500

    Proyecto inicializado - Estructura Spring + docker-compose

---
+++ src/test/java/com/jp/apm/ApmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jp.apm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
